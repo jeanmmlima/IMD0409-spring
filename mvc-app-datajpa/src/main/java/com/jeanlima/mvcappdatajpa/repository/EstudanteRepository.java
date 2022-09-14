0cacbea354ccd37cf9391fd0c6d84580db5e8787
jeanmmlima <jeanmariomlima@gmail.com>
2022-09-14 17:12:42 -0300

    query methods no jpa repository

--- mvc-app-datajpa/src/main/java/com/jeanlima/mvcappdatajpa/repository/EstudanteRepository.java
+++ mvc-app-datajpa/src/main/java/com/jeanlima/mvcappdatajpa/repository/EstudanteRepository.java
@@ -3,6 +3,7 @@ package com.jeanlima.mvcappdatajpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,6 +12,24 @@ import com.jeanlima.mvcappdatajpa.model.Estudante;
 
 public interface EstudanteRepository extends JpaRepository<Estudante,Integer>{
 
-    
+    List<Estudante> findByNomeLike(String nome);
+    List<Estudante> findByNomeOrId(String nome, Integer id);
+    boolean existsByNome(String nome);
+
+    /*
+     * Trabalhando com @Query
+     */
+    //hql
+    @Query(value = " select c from Estudante c where c.nome like %:nome% ")
+    List<Estudante> encontrarPorNome(@Param("nome") String nome);
+
+    //sql nativo
+    @Query(value = " select * from estudante c where c.nome like %:nome% ",nativeQuery = true)
+    List<Estudante> encontrarPorNomeMod(@Param("nome") String nome);
+
+    @Query(value = " delete from Estudante c where c.nome =:nome")
+
+    @Modifying //pois não é só consulta - transactional 
+    void deletarPorNome(String nome);
 
 }
